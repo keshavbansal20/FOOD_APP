f63e0f567296b43be9ab514a7d8d242fd56c852e
Keshav Bansal <bansal.keshav.mathura@gmail.com>
2021-04-26 23:45:30 +0530

    added config secret file

---
+++ config/secret.js
@@ -0,0 +1,6 @@
+module.exports =  {
+    SECRET_KEY:"wqazedcxswydgeyuihgntvmotmgmm9uh78y3m3uhfndshlds133mdklmdknjnakoi",
+    GMAIL_ID:"bansal.keshav.mathura@gmail.com",
+    GMAIL_PW:"fdjhwcobwmzzhphj",
+}
+
